ca1da6dee538fca76f16acfb4f417c2512ccceaa
boissonnfive <boissonnfive@gmail.com>
2016-09-29 23:11:51 +0200

    Copie les dossiers dans le dossier packges de ST2.

--- Synchronise_ST2.bat
+++ Synchronise_ST2.bat
@@ -1,11 +1,15 @@
 @echo off
 
 set ST2_PACKAGE_FOLDER=%APPDATA%\Sublime Text 2\Packages
+:: 
+:: copy User "%ST2_PACKAGE_FOLDER%\User"
+:: copy VBScript "%ST2_PACKAGE_FOLDER%\VBScript"
+:: copy "Batch File" "%ST2_PACKAGE_FOLDER%\Batch File"
 
-copy User "%ST2_PACKAGE_FOLDER%\User"
 
-
-:: for %%i in (*) do echo %%i
-:: (
-::	copy %s 
-:: )
+::for "delims=," %%i in (User,VBScript,Batch File) do echo "%ST2_PACKAGE_FOLDER%\%%i"
+::FOR /F "tokens=*" %%i IN ('dir /AD-H /b') do echo %%i
+FOR /F "tokens=*" %%i IN ('dir /AD-H /b') do (
+	::echo %%i
+	copy "%%i" "%ST2_PACKAGE_FOLDER%\%%i" 
+)
